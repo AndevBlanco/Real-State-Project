64bc71dd648c5861218f0bf53ce34287dcf88e88
Andres Blanco <39788602+AndresBlanco7@users.noreply.github.com>
2019-11-14 12:08:59 -0500

    Add files via upload
    Base de datos en mysql

---
+++ inalpes.sql
@@ -0,0 +1,2 @@
+SELECT * FROM inalpes.estate;
+INSERT INTO inalpes.estate (precio, area, habitaciones, baños, garajes, lugar, proceso, tipo) VALUES ('2', '2', '2', '2', '2', 'bogota', 'comprarnuevo', 'apartemento');
