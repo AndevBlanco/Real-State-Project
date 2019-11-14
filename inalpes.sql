a8cde682920f760617499cbf73012e76a1ec5e44
Andres Blanco <39788602+AndresBlanco7@users.noreply.github.com>
2019-11-14 12:28:06 -0500

    Delete inalpes.sql

--- inalpes.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM inalpes.estate;
-INSERT INTO inalpes.estate (precio, area, habitaciones, baños, garajes, lugar, proceso, tipo) VALUES ('2', '2', '2', '2', '2', 'bogota', 'comprarnuevo', 'apartemento');
